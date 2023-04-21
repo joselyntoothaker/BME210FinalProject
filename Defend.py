b2def8b59a962e9bce0429c1970f16507b3778b0
joselyntoothaker <94273285+joselyntoothaker@users.noreply.github.com>
2023-04-21 10:30:46 -0700

    Update Defend.py

--- Defend.py
+++ Defend.py
@@ -2,12 +2,14 @@ from pynput.keyboard import Listener
 import meArm
 import time
 
-arm = meArm.meArm() # takes inserted data from meArm.py aka calibration data
-arm.begin(0,0x70) #
 
-# Start Position
+arm = meArm.meArm() # takes inserted data from meArm.py aka calibration data
+arm.begin(0,0x70) #takes the rpoper i2c bus input
+
+# defending Position
 xs = 5 # x coordinate
 ys = 135 # y coordinate
 zs =30 # z coordinate
 
+#goes towards stationary defending position
 arm.gotoPoint(xs,ys,zs) 
