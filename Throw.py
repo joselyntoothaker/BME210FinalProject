d22fc5173fcfb9719cfae25ecd984950c73bd79d
mckennanichols12 <129898040+mckennanichols12@users.noreply.github.com>
2023-05-01 10:11:35 -0700

    Update Throw.py

--- Throw.py
+++ Throw.py
@@ -3,9 +3,9 @@ import meArm
 import time
 
 arm = meArm.meArm() # takes inserted data from meArm.py aka calibration data
-arm.begin(0,0x70) #
+arm.begin(0,0x70) #locating
 
-#;Start Position
+#Start Position
 xs = 90 # x coordinate;;
 ys = -6 # y coordinate
 zs = 108 # z coordinate
@@ -14,26 +14,21 @@ xe = 80 # x coordinate
 ye = 144 # y coordinate
 ze = 105 # z coordinate
 
-arm.gotoPoint(xs,ys,zs)
-#time.sleep(5)
-#arm.gotoPoint(xe,ye,ze)
-#arm.goDirectlyTo(xe,ye,ze)
-#arm.closeGripper()
+arm.gotoPoint(xs,ys,zs) #goes to start position
 
 def on_press(key):
     global xs,ys,zs, xe,ye,ze
     var = str(format(key))
-    semi = '\';\''
+    semi = '\';\'' #read semi colon to run the throw sequence
  
     if var == semi: ## PARIALLY opens gripper, to percentage (inputed, default is 50% but this can be modified) of full open state when ";" key is pressed
-        arm.goDirectlyTo(xe,ye,ze)
-        time.sleep(5.)
-        arm.gotoPoint(xs,ys,zs) 
-           
+        arm.goDirectlyTo(xe,ye,ze) #goes to end point to throw
+        time.sleep(5)
+        arm.gotoPoint(xs,ys,zs) #moves back to beginning to allow for another throw
     pass
    
-def on_release(key):
-    pass
+def on_release(key): #reas the release of the key
+    pass 
   
 with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
