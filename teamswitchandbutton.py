fb2080b77f9ecfc8face24527dfc92aee84eea84
mckennanichols12 <129898040+mckennanichols12@users.noreply.github.com>
2023-05-01 10:08:00 -0700

    Update teamswitchandbutton.py

--- teamswitchandbutton.py
+++ teamswitchandbutton.py
@@ -5,54 +5,38 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 arm = meArm.meArm() # takes inserted data from meArm.py aka calibration data
-arm.begin(0,0x70) #
-switch = 17
-button = 27
+arm.begin(0,0x70) # locating
+switch = 17 #GPIO pin for the switch
+button = 27 #GPIO pin for the button
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Start Position
 xs = 130 # x coordinate
 ys = -30 # y coordinate
-zs = 100# z coordinate
+zs = 100 # z coordinate
 # End Position
 xe = 80 # x coordinate
 ye = 144 # y coordinate
 ze = 105 # z coordinate
 
-arm.gotoPoint(xs,ys,zs) 
+arm.gotoPoint(xs,ys,zs) #initial position the arm defaults to before the button is pressed
 arm.closeGripper()
-def throwing():
+def throwing(): #function for the offensive mode
     print("THROWING MODE")
-#     def on_press(key):
-#         global xs,ys,zs, xe,ye,ze
-#         var = str(format(key))
-#         semi = '\';\''
-            
-        #if var == semi: ## PARIALLY opens gripper, to percentage (inputed, default is 50% but this can be modified) of full open state when ";" key is pressed
-    arm.goDirectlyTo(xe,ye,ze) 
-    time.sleep(0.5)
-    arm.gotoPoint(xs,ys,zs) 
+    arm.goDirectlyTo(xe,ye,ze) #throws the ball to end position
+    time.sleep(0.5) #waits
+    arm.gotoPoint(xs,ys,zs) #returns to initial position
     state = switch()        
     pass
 
-#     def on_release(key):
-#         pass
-
-#     with Listener(on_press=on_press, on_release=on_release) as listener:
-#         listener.join()
-def defending():
+def defending(): #function for the defensive mode
     print("DEFENDING MODE")
-    #def on_press(key):
-        #global xs,ys,zs, xe,ye,ze
-        #var = str(format(key))
-        #semi = '\';\''
-        
-        #if var == semi: ## PARIALLY opens gripper, to percentage (inputed, default is 50% but this can be modified) of full open state when ";" key is pressed
-    arm.goDirectlyTo(13,153,29) 
+    arm.goDirectlyTo(13,153,29) #moves arm to this position
     time.sleep(0.5)
     arm.gotoPoint(13,153,29)
-    while True:
+    while True: #while the switch is in the designated position
+        #the mearm will move between the following two points within the goal
         time.sleep(0.2)
         arm.gotoPoint(53,153,29)
         time.sleep(0.2)
@@ -61,31 +45,25 @@ def defending():
             state = switch();    
     pass
 
-#     def on_release(key):
-#         pass
-
-#     with Listener(on_press=on_press, on_release=on_release) as listener:
-#         listener.join()
-
-def switch():
+def switch(): #reads the changes in the switch
     arm.gotoPoint(xs,ys,zs)
     print("SWITCH MODE")
-    while True:
-        if GPIO.input(17):
+    while True: 
+        if GPIO.input(17): #if its in this position it is the throwing code
             print("Switch = Throwing")
-            while GPIO.input(button) == False:
+            while GPIO.input(button) == False: #waits until button is pressed
                 print("Not Pressed")
-                if GPIO.input(button) == True:
+                if GPIO.input(button) == True: #reads the button press
                     time.sleep(0.01)
-                    state = throwing()
+                    state = throwing() #runs the throw code in situations when the button is pressed until there is a change in switch position
                 break
-        if not GPIO.input(17):
+        if not GPIO.input(17): #if the button is in the other position
             print("Switch = Defending")
-            while GPIO.input(button) == False:
+            while GPIO.input(button) == False: #waits until button is pressed
                 print("Not Pressed")
-                if GPIO.input(button) == True:
+                if GPIO.input(button) == True: #reads the button press
                     print("Pressed")
                     time.sleep(0.01)
-                    state = defending()
+                    state = defending() #runs the defending code until there is a change in switch position
                 break
 state = switch()
